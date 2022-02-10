4fc340bd1afb364f3ee6839c0033ec49f61265b3
yoo chang heon <dbckdgjs1130@naver.com>
2022-02-10 22:07:49 +0900

    [feat] 점수 계산 로직 추가. 시간이 줄어들 수록 더 많은 점수 얻게 했음

--- src/pages/MiniGamePage/MiniGamePage.jsx
+++ src/pages/MiniGamePage/MiniGamePage.jsx
@@ -3,23 +3,26 @@ import BlockBox from "../../components/BlockBox/BlockBox";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 import { LowerDiv, UpperDiv } from "./styled";
 
-export default function MiniGamePage() {
+export default function MiniGamePage(props) {
   const [time, setTime] = useState(500);
   const [score, setScore] = useState(0);
 
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setTime((time) => time - 500 / 60);
-  //   }, 1000);
-  //   if (time < 0) {
-  //     alert(`GAME OVER!\n 점수: ${score}`);
-  //     clearInterval(timer);
-  //     setScore(0);
-  //     setTime(500);
-  //   }
-  //   return () => clearInterval(timer);
-  // }, [time]);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((time) => time - 500 / 60);
+    }, 1000);
+    if (time < 0) {
+      alert(`GAME OVER!\n 점수: ${score}`);
+      clearInterval(timer);
+      setScore(0);
+      setTime(500);
+    }
+    return () => clearInterval(timer);
+  }, [time]);
 
+  const calScore = (number) => {
+    setScore((score) => score + number * Math.floor(100 - time / 60));
+  };
   return (
     <div>
       Score:{score}
@@ -31,7 +34,7 @@ export default function MiniGamePage() {
       <br />
       <br />
       <LowerDiv>
-        <BlockBox></BlockBox>
+        <BlockBox func={calScore}></BlockBox>
       </LowerDiv>
     </div>
   );
